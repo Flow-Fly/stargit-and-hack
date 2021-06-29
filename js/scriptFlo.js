658f6bcd3a69faca693cc3b78bd9b2b013dd6985
Flow-Fly <florian.aube@gmail.com>
2021-06-29 12:33:21 +0200

    Changed to a concatenated string

--- js/scriptFlo.js
+++ js/scriptFlo.js
@@ -1,12 +1,10 @@
 
-
 let circle = document.getElementById('circle')
 
 window.addEventListener('mousemove', (position) => {
 
     let center = {x: position.view.innerWidth / 2, y: position.view.innerHeight / 2};
-    let limit = {x: 1, y: 1};
-    
+    let limit = {x: 1, y: .7};
     let Xprojection = (center.x - position.x) / 500;
     let Yprojection = (center.y - position.y) / 500;
 
@@ -22,8 +20,11 @@ window.addEventListener('mousemove', (position) => {
     if(Yprojection < (limit.y * -1)) {
         Yprojection = limit.y * -1;
     };
-    
-    let changeBoxShadow = `${0.7 * Xprojection}em ${0.7 * Yprojection}em 5px #F5A962, ${Xprojection}em ${Yprojection}em 10px  #125D98, ${1.3 * Xprojection}em ${1.3 * Yprojection}em 17px rgb(172, 172, 172);`;
-    circle.style['boxShadow'] = changeBoxShadow;
+    let changeBoxShadow = (0.7 * Xprojection).toFixed(2) + "px " + (0.7 * Yprojection).toFixed(2) + "px 5px #F5A962, " + Xprojection.toFixed(2) + "px " + Yprojection.toFixed(2) + "px 10px #125D98, " + (1.3 * Xprojection).toFixed(2) + "px " + (1.3 * Yprojection).toFixed(2) + "px 17px rgb(172, 172, 172);";
+    //let changeBoxShadow = `${(0.7 * Xprojection).toFixed(2)}px ${(0.7 * Yprojection).toFixed(2)}px 5px #F5A962, ${Xprojection.toFixed(2)}px ${Yprojection.toFixed(2)}px 10px #125D98, ${(1.3 * Xprojection).toFixed(2)}px ${(1.3 * Yprojection).toFixed(2)}px 17px rgb(172, 172, 172);`;
     console.log(changeBoxShadow)
+    //console.log(changeBoxShadow)
+    //circle.style['boxShadow'] = changeBoxShadow;
+    circle.style.boxShadow = changeBoxShadow;
+    //console.log(changeBoxShadow)
 })
